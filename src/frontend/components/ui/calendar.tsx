bfcf83466065bc3295369acd1b1dc5f4c55b8a57
papettoTV <aegfrompsbt@gmail.com>
2026-02-11 06:41:47 +0900

    react-day-pickerのversion依存関係対応

--- src/frontend/components/ui/calendar.tsx
+++ src/frontend/components/ui/calendar.tsx
@@ -54,8 +54,11 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        Chevron: (chevronProps) => {
+          const Icon =
+            chevronProps.orientation === "left" ? ChevronLeft : ChevronRight
+          return <Icon className="h-4 w-4" />
+        },
       }}
       {...props}
     />
